2c9562af97974a0c5d2207ae70e002f0c219d87f
Dm1trySt <dim139@mail.ru>
2019-10-19 14:17:33 +0300

    methods

---
+++ sklonenie.rb
@@ -0,0 +1,36 @@
+def sklonenie(number, krokodil, krokodila, krokodilov)
+  #Проверка введенных данных
+  # класс Numeric - класс, которому принадлежат все числа
+  # он проверяет: НЕ(!) принадлжит ли number этому классу
+  if (number == nil || !number.is_a?(Numeric))
+    number = 0
+  end
+
+  ostatok = number % 10
+
+  if ostatok == 1
+    return krokodil
+  elsif ostatok >=2 && ostatok <=4
+    return krokodila
+  elsif ostatok > 4 || ostatok == 0
+    return krokodilov
+  end
+end
+
+skolko = ARGV[0]
+#если не был передан параметр спрашиваем
+# у пользователя количество
+if skolko == nil
+  puts"Введите количество:"
+  skolko = STDIN.gets.to_i
+end
+#если даже пользователь введет не корректные данные
+# .to_i передает 0 в skolko, поэтому проверка в методе
+# нужна только как пример
+skolko = skolko.to_i
+
+#вывод
+puts "#{skolko} " +
+  sklonenie(skolko,"кот","кота","котов") +
+    sklonenie(skolko," пошел"," пошли"," пошли") +
+    " купаться в море!"
